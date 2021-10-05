6137cd5d8acbc883f9413b35e6cc6ec18b646a0d
grommeo <rpikunov@gmail.com>
2021-10-05 18:08:55 +0800

    Добавьте файлы проекта.

---
+++ Лаба 2-2 Сsh/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Лаба_2_2_Сsh
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.Text = Properties.Settings.Default.pr;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string pr;
+            pr = Convert.ToString(textBox1.Text);
+            var Out = Logic.Doli(pr);
+            label4.Text = Out;
+            Properties.Settings.Default.pr = pr;
+            Properties.Settings.Default.Save();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+        }
+    }
+    public class Logic
+    {
+        public static string Doli(string pr)
+        {
+            float buk = 0, m = 0, dl = 0;
+            string Out, a, b, c;
+            for (int i = 0; i < pr.Length; i++)
+            {
+                dl++;
+                if ((pr[i] >= 'а' && pr[i] <= 'я') || (pr[i] >= 'А' && pr[i] <= 'Я'))
+                    buk++;
+
+            }
+            m = buk / dl * 100;
+            Out = "Букв: " + buk.ToString() + "\nСимволов: " + dl.ToString() + "\nДоля: " + m.ToString() + "%";
+            return (Out);
+        }
+    }
+}
---
+++ Лаба 2-2 СshTests/LogicTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Лаба_2_2_Сsh;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Лаба_2_2_Сsh.Tests
+{
+    [TestClass()]
+    public class LogicTests
+    {
+        [TestMethod()]
+        public void DoliTest1()
+        {
+            string pr = "Привет 21344";
+
+            var Out = Logic.Doli(pr);
+
+            Assert.AreEqual("Букв: 6\nСимволов: 12\nДоля: 50%", Out);
+        }
+        [TestMethod()]
+        public void DoliTest2()
+        {
+            string pr = "2134324";
+
+            var Out = Logic.Doli(pr);
+
+            Assert.AreEqual("Букв: 0\nСимволов: 7\nДоля: 0%", Out);
+        }
+        [TestMethod()]
+        public void DoliTest3()
+        {
+            string pr = "Привет";
+
+            var Out = Logic.Doli(pr);
+
+            Assert.AreEqual("Букв: 6\nСимволов: 6\nДоля: 100%", Out);
+        }
+        [TestMethod()]
+        public void DoliTest4()
+        {
+            string pr = "м        м";
+
+            var Out = Logic.Doli(pr);
+
+            Assert.AreEqual("Букв: 2\nСимволов: 10\nДоля: 20%", Out);
+        }
+    }
+}
